43833136fa7d3b63e3bba0fbc1a2ab709f1a73f4
Abirami S M <93912144+Abirami1921001@users.noreply.github.com>
2022-11-19 19:55:13 +0530

    Create python program, to display prime numbers series upto given number.py

---
+++ Assignments/Abirami S M TL/Assignment 1/python program, to display prime numbers series upto given number.py
@@ -0,0 +1,8 @@
+upper = int(input()) 
+for n in range(1, upper + 1):
+      if n > 1:
+       for i in range(2, n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)
